dbe8927a20f4beb701329db0772c1fcabcc57d7e
MoLines Designs <admin@molinesdesigns.com>
2026-04-24 11:57:55 +0200

    brewbar 0.3.1

--- Casks/brewbar.rb
+++ Casks/brewbar.rb
@@ -1,6 +1,6 @@
 cask "brewbar" do
-  version "0.3.0"
-  sha256 "f073323911271b1130687e085c13d0521c920a23121c7820b538575fe76e9794"
+  version "0.3.1"
+  sha256 "2161fe509781980f9e943cccd91c37b3de73eab1dcdae6a3d557736facd3fd9e"
 
   url "https://github.com/MoLinesGitHub/Brew-TUI/releases/download/v#{version}/BrewBar.app.zip"
   name "BrewBar"

2aff728cab5f5558edef0b8e9259541b77d80f1d
MoLines Designs <admin@molinesdesigns.com>
2026-05-01 13:58:17 +0200

    brew-tui 0.6.1

--- Formula/brew-tui.rb
+++ Formula/brew-tui.rb
@@ -1,8 +1,8 @@
 class BrewTui < Formula
   desc "Visual TUI for Homebrew package management"
   homepage "https://github.com/MoLinesDesigns/Brew-TUI"
-  url "https://registry.npmjs.org/brew-tui/-/brew-tui-0.6.0.tgz"
-  sha256 "8f5a0d5ec5789a14b9bfd44cc4cfc9c4682805ed39cebcc241a0b2773f2d28b9"
+  url "https://registry.npmjs.org/brew-tui/-/brew-tui-0.6.1.tgz"
+  sha256 "7ff9750d3b7eced472619c4434043eeef6bafabac2160d3e8ba947fc40bed530"
   license "MIT"
 
   depends_on "node@22"
